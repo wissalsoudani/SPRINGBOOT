73aa6259b847c62d4b302900dc371b5c333ced1d
wissalsoudani <wissal.soudani@esprit.tn>
2022-12-10 22:57:04 +0100

    test

--- src/main/java/tn/esprit/aspects/PerformanceAspect.java
+++ src/main/java/tn/esprit/aspects/PerformanceAspect.java
@@ -21,8 +21,6 @@ public class PerformanceAspect {
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
-//	    List<Object> obj = new ArrayList<Object>(); // si le type de retour est une liste
-//		obj = (List<Object>) pjp.proceed();
 		Object obj = pjp.proceed();
 
 		stopWatch.stop();
